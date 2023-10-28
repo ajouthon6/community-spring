d20413bb9fc627745e96dddf2d2d910c5bb1693d
Bandall <jsm5315@naver.com>
2023-10-29 03:33:57 +0900

    fix: delete 버그 픽스, body type => MIDEUMTEXT

--- src/main/java/com/ajousw/spring/domain/comment/CommentJpaRepository.java
+++ src/main/java/com/ajousw/spring/domain/comment/CommentJpaRepository.java
@@ -2,6 +2,7 @@ package com.ajousw.spring.domain.comment;
 
 import com.ajousw.spring.domain.board.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -11,6 +12,7 @@ public interface CommentJpaRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> findByBoardId(Long boardId);
 
+    @Modifying(clearAutomatically = true)
     @Query("delete from Comment where board=:board")
     void deleteAllByBoardId(@Param("board") Board board);
 }
