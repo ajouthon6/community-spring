8f0ae0908970174bd241a568e96360aa6b487eaa
Bandall <jsm5315@naver.com>
2023-10-29 06:06:20 +0900

    fix: presentation Param으로 변경

--- src/main/java/com/ajousw/spring/domain/challenger/Challenger.java
+++ src/main/java/com/ajousw/spring/domain/challenger/Challenger.java
@@ -1,27 +1,18 @@
 package com.ajousw.spring.domain.challenger;
 
-import com.ajousw.spring.domain.board.Board;
-import com.ajousw.spring.domain.member.repository.BaseTimeEntity;
-import com.ajousw.spring.domain.member.repository.Member;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Entity
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Challenger extends BaseTimeEntity {
-
-
-    @JoinColumn(name = "member_id")
-    private Board board;
-
-    private Member member;
-
-    private String message;
-
-    private Boolean isChecked;
-
-}
+//@Entity
+//@Getter
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//public class Challenger extends BaseTimeEntity {
+//
+//
+//    @JoinColumn(name = "member_id")
+//    private Board board;
+//
+//    private Member member;
+//
+//    private String message;
+//
+//    private Boolean isChecked;
+//
+//}
--- src/main/java/com/ajousw/spring/web/controller/PresentationController.java
+++ src/main/java/com/ajousw/spring/web/controller/PresentationController.java
@@ -3,7 +3,6 @@ package com.ajousw.spring.web.controller;
 import com.ajousw.spring.domain.member.security.UserPrinciple;
 import com.ajousw.spring.domain.presentation.PresentationService;
 import com.ajousw.spring.web.controller.dto.presentation.CreatePresentationDto;
-import com.ajousw.spring.web.controller.dto.presentation.GetPresentationDto;
 import com.ajousw.spring.web.controller.dto.presentation.PresentationDto;
 import com.ajousw.spring.web.controller.dto.presentation.UpdatePresentationDto;
 import com.ajousw.spring.web.controller.json.ApiResponseJson;
@@ -12,10 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -41,11 +37,11 @@ public class PresentationController {
         return new ApiResponseJson(HttpStatus.OK, "OK");
     }
 
-    @GetMapping("/presentation/get")
-    public ApiResponseJson getPresentation(@RequestBody @Valid GetPresentationDto getPresentationDto) {
+    @GetMapping("/presentation/get/{email}")
+    public ApiResponseJson getPresentation(@PathVariable String email) {
         log.info("[GET-PRESENTATION]");
 
-        PresentationDto presentationDto = presentationService.getPresentation(getPresentationDto.getEmail());
+        PresentationDto presentationDto = presentationService.getPresentation(email);
 
         return new ApiResponseJson(HttpStatus.OK, presentationDto);
     }
